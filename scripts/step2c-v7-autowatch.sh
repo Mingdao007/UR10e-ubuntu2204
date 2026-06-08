50a9c03052cd2ef2f24deea4b967b0da6794655d
Mingdao007 <25086082r@connect.polyu.hk>
2026-06-08 15:01:32 +0800

    Add Step2C v7 accel300 near3 program

---
+++ scripts/step2c-v7-autowatch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+EXP_DIR="/home/andy/ur10e_ros2_ws/experiments/kunwei/closed-loop-straight-line/2026-06-04"
+
+exec "${EXP_DIR}/scripts/step2c-admittance-search30-v7-accel300-near3-operator.sh" autowatch
