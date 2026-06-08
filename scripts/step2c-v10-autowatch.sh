cff2272d3b51ede581bf368c374898678e38847c
Mingdao007 <25086082r@connect.polyu.hk>
2026-06-08 15:26:10 +0800

    Add Step2C v10 soft acquisition tuning

---
+++ scripts/step2c-v10-autowatch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+EXP_DIR="/home/andy/ur10e_ros2_ws/experiments/kunwei/closed-loop-straight-line/2026-06-04"
+
+exec "${EXP_DIR}/scripts/step2c-admittance-search15-v10-v9soft3-operator.sh" autowatch
